5d8dbc23bc4b83aa3a71aa6b14b454f9f07e8283
relena <era8411@nyu.edu>
2026-03-23 21:51:16 -0400

    changed background and fixed issues with sound

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ let abby = {
   img: null
 };
 
+let bg;
 let osc1, osc2 
 
 let freq = 220; 
@@ -11,38 +12,43 @@ let freqMod = 0;
 
 function preload(){
   abby.img = loadImage('images/abbyleemiller.jpg'); 
+  bg = loadImage('images/club.jpg');
 }
 
 function setup() {
   createCanvas(windowWidth, windowHeight); 
-  abby.img.resize(120,120)
-  osc1 = new p5.Oscillator('triangle')
-  osc2 = new p5.Oscillator('triangle')
+  
+  if (abby.img) {
+    abby.img.resize(120,120)
+  }
+
+  osc1 = new p5.Oscillator('square')
+  osc2 = new p5.Oscillator('sawtooth')
   
   osc1.start()
   osc2.start()
-
 
   osc1.amp(0)
   osc2.amp(0)
 } 
 
 function draw() {
-  
-background(0,0,0); 
+  image(bg, 0, 0, width, height);
 
-  
   abby.x = noise(frameCount/100)*width
   abby.y = noise(frameCount/100+100)*height
   
-  image(abby.img,abby.x,abby.y)
+  image(abby.img, abby.x, abby.y)
   
-  freqMod = map(abby.x, 0,abby.y, -15,15)
+  freqMod = map(abby.x, 0, width, -15, 15)
   
   osc1.freq(freq)
   osc2.freq(freq + freqMod)
-  osc1.amp(0.5)
-  osc2.amp(0.5)
- 
-  }
- 
+
+  osc1.amp(0.5, 0.1)
+  osc2.amp(0.5, 0.1)
+}
+
+function mousePressed() {
+  userStartAudio();
+}
